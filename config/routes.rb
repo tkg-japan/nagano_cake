d8f9f7057522dabf813ad4d83fe8885b2a60e589
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-16 09:50:58 +0000

    [Add]public_controller1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+  get 'addresses/edit'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/unsubscribe'
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
