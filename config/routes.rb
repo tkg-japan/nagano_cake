b6bbe9909ffdc8a1b0e4ad142c1c9d67a4a2d46a
EC2 Default User <ec2-user@ip-172-31-84-169.ec2.internal>
2021-12-16 09:51:20 +0000

    [add] public-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  get 'cart_items/index'
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
+  get 'orders/complete'
+  get 'items/index'
+  get 'items/show'
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
