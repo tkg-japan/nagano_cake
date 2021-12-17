c835a5347b508305350e41913af7f04d1a54af62
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-12-17 12:09:30 +0000

    [Add]admin.sign-in

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Admin::SessionsController < Devise::SessionsController
+class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,14 +24,4 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-
-  protected
-  def after_sign_in_path_for(resource)
-    user_path(resource)
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
 end
