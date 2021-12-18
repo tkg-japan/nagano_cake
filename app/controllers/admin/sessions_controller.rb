a2cae6cc5f0497dce225b9b4f42f52a6418322df
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-12-18 07:08:31 +0000

    [Add]管理人データ登録

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,7 +27,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   protected
   def after_sign_in_path_for(resource)
-    user_path(resource)
+    admin_items_path(resource)
   end
 
   def after_sign_out_path_for(resource)
