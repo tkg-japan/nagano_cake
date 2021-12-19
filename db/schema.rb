1717c4e7eb22ea6499a33ff476e739bb001144ae
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-19 05:06:39 +0000

    [Add]customer.sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -47,15 +47,16 @@ ActiveRecord::Schema.define(version: 2021_12_18_084838) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
     t.string "first_name"
     t.string "last_name_kana"
     t.string "first_name_kana"
     t.string "postcode"
     t.string "phone_number"
-    t.string "Last_name"
+    t.string "last_name"
+    t.string "address"
+    t.boolean "is_deleted", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
