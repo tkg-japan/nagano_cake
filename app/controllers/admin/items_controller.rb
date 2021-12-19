69440eb947599ad0fb7ba95cacfd70d48c095bcd
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-12-19 10:45:49 +0000

    [add]new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,27 @@
 class Admin::ItemsController < ApplicationController
+
+
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
-  
-  def inde
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path(@item.id), notice: "いいーね!!"
+    else
+      # frash.now[:alert] = "もう一度入力し直してください。"
+      render "index"
+    end
+
+  end
+
+  def updated
+  end
+
+  def index
+    @items = Item.all
   end
 
   def show
@@ -10,15 +29,13 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
-  
-  def create
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image_id, :price, :genre_id, :is_active)
   end
 
-
-  def updated
-  end
-
-  
-  
-  
 end
