2adbb159e446f5eb0e7ce35e603db8acee650953
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-20 08:35:41 +0000

    [Finish]

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -30,11 +30,9 @@ class Customer::SessionsController < Devise::SessionsController
 
   def reject_inactive_customer
     @customer = Customer.find_by(email: params[:customer][:email])
-      return if !@customer
-
-        if @customer.is_deleted_password?(params[:customer][:password])
-          redirect_to new_customer_session_path
-        end
-
+    return if !@customer
+    if (@customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true))
+      redirect_to new_customer_registration_path
+    end
   end
 end
