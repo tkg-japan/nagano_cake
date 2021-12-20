b558b68db9e3ff2107988c8698b47ef8a77257ba
Sao95 <merasaori@gmail.com>
2021-12-20 09:25:33 +0000

    [Add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @orders = Order.all
+  end
+  
   def show
+    @customer = Customer.find(params[:id])
+    @orders = Order.all
   end
   
   def update
