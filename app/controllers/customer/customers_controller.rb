e2bfa378df60d8af74e474270fe0c7c8290e3a10
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-20 04:03:12 +0000

    [Add]

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -10,6 +10,10 @@ class Customer::CustomersController < ApplicationController
 
   def unsubscribe
     @customer = Customer.find_by(name: params[:email])
+  end
+
+  def withdraw
+    @customer = Customer.find_by(name: params[:email])
     @customer.update(is_valid: false)
     reset_session
     redirect_to root_path
