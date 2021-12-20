2deba35bf4e882f0018372abb35c5e68c5dd7377
EC2 Default User <ec2-user@ip-172-31-84-169.ec2.internal>
2021-12-20 07:22:34 +0000

    [add]customer

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -2,10 +2,22 @@ class Customer::CustomersController < ApplicationController
 
 
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "登録情報を編集しました。"
+      redirect_to customers_path(current_customer)
+    else
+      flash[:alert] = "もう一度入力してください。"
+      render :customer_edit
+    end
   end
 
   def unsubscribe
@@ -17,5 +29,11 @@ class Customer::CustomersController < ApplicationController
     @customer.update(is_valid: false)
     reset_session
     redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
   end
 end
