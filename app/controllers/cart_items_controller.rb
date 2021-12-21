de32d4b45047a74238b09f8a4159fc0f6dd849be
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-21 08:22:24 +0000

    [Add]

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,11 +3,17 @@ class CartItemsController < ApplicationController
   end
 
   def create
+    
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
