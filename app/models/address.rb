14dd19f6d0935f1dd3977e4273f6597d04d11871
EC2 Default User <ec2-user@ip-172-31-84-169.ec2.internal>
2021-12-21 04:34:26 +0000

    [add]errormessage

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 class Address < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
