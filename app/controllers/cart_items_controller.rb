4eafea7d8b18a4a9fb453c748ff3492dbef05c21
Sao95 <merasaori@gmail.com>
2021-12-21 08:27:55 +0000

    [Add] 会員側のカート内商品

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,21 +2,38 @@ class CartItemsController < ApplicationController
   
   before_action :authenticate_customer!
   
-  def index
-    @cart_items = CartItem.all
-    @order = Order.new
-    
-    # @total = 0
-    # @cart_products.all.sum(:quantity)
-    # @products = CartProduct.all
-  end
-
   def create
+  end
+  
+  def index
+    @cart_items = current_customer.cart_items.all
+    @total = 0
   end
 
   def update
+    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_item.update(cart_item_params)
+      # 遷移元のURLを取得
+      redirect_to request.referer
   end
 
   def destroy
+    @cart_items = current_customer.cart_items.find(params[:id])
+    @cart_items.destroy
+    # 遷移元のURLを取得
+    redirect_to request.referer
   end
+  
+  def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to request.referer
+  end
+  
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,10 @@ class CartItem < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
+
+  # 小計を求めるメソッド・カリキュラム参照
+  def subtotal
+    item.with_tax_price * amount
+  end
   
 end
