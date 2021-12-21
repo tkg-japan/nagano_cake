03495699347de8ed67f1bafd9adc5704295b83aa
Sao95 <merasaori@gmail.com>
2021-12-21 03:54:21 +0000

    [Add]管理者側の注文履歴

--- app/controllers/admin/order_datails_controller.rb
+++ app/controllers/admin/order_datails_controller.rb
@@ -1,4 +1,39 @@
 class Admin::OrderDatailsController < ApplicationController
-  def update
+  
+  before_action :authenticate_admin!
+
+  def index
+    @order = Order.find_by(params[:id])
+    @order_details = @order.order_details
   end
+  
+  def update
+    # making_statusでどのボタンが押されたかを特定する
+    @order_detail = OrderDetail.find(params[:id])
+    # 注文商品から注文テーブルの呼び出し
+    @order = @order_detail.order
+    # 製作ステータスの更新はmaking_statusだけのデータ取得が必要。to_iではいってくるデータをintegerにする
+    # tkg-jはto_iメソッド使う？
+    @order_detail.update(production_status: order_detail_params[:making_status].to_i)
+
+    # p @order_detail ←？？
+    if @order_detail.making_status == "製作中"
+      # 注文ステータスを"製作中"に更新
+      @order.update(order_status: 2)
+      
+    elsif @order_detail.making_status == "製作完了"
+    # elsif @order.order_details.count == @order.order_details.where(making_status: 3).count
+      # 注文ステータスを"発送準備中"に更新
+      @order.update(order_status: 3)
+    end
+    # 遷移元のURLを取得
+    redirect_to request.referer
+  end
+  
+  # private
+  
+  # def order_detail_params
+  #   params.require(:order_detail).permit(:production_status)
+  # end
+  
 end
