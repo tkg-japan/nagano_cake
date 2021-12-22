02621f01b7f6663e4814746173518e942c74c029
Sao95 <merasaori@gmail.com>
2021-12-22 09:35:08 +0000

    [Add]

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   enum payment_method: { credit_card: 0, transfer: 1}
   # 0が入っている場合は credit_card  1が入ってる場合はtransfer(銀行振込)
-
-
+  
+  enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 end
